bf13335404317a29decd2284fb4d782aa72533b0
KundanGuptaa <kundankumarsobh@gmail.com>
2025-07-13 10:58:15 +0530

    13thJul2025Practice

--- 33. Backend 2 (Node - Express)/ExpressDir/index.js
+++ 33. Backend 2 (Node - Express)/ExpressDir/index.js
@@ -1,12 +1,45 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 
-let port=8080; app.listen(port, ()=>{
+let port = 8080;
+app.listen(port, () => {
     console.log(`app is lsitening on port ${port}`);
 });
+// app.use((req,res)=>{
+//     console.log("Request Received");
+//     res.send({
+//         name:"apple",
+//         color:"red"
+//     })
+// })
 
-app.get("/",(req,res)=>{
-    res.send("hello, i am root");
+// app.get("/kundan", (req, res) => {
+//     res.send("hello, i am kundan");
+// });
+
+// app.get("/", (req, res) => {
+//     res.send("hello, i am root");
+// });
+
+// app.get("*name", (req, res) => {
+//     res.send("404 Page not found!");
+// });
+// app.post("/",(req,res)=>{
+//     res.send("You send post request to root.")
+// })
+
+app.get("/:username/:id",(req,res)=>{
+    let {username,id}=req.params;
+    console.log(req.params);
+    res.send(`Welcoms to the page of @${username}.`);
 })
 
-app.get("/:username")
+app.get("/search",(req,res)=>{
+    let {q}=req.query;
+    console.log(req.query);
+    if(!q){
+        res.send("<h1>Nothing Searched</h1>");
+    }
+    let htmlstr=`<h1>These are the search result for ${q}</h1>`;
+    res.send(htmlstr);
+})
