671de2f1d6d21e11707179b6fa1422422195660d
Kundan Gupta <kundankumarsobh@gmail.com>
2025-07-15 14:22:37 +0530

    Learned about Get & Post Requests

---
+++ 35. Miscellaneous(Get&Post_Request)/BACKEND/index.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const app=express();
+const port=8080;
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+app.get("/register",(req,res)=>{
+    let {user,pass}=req.query;
+    res.send(`Standard GET Response.Welcome ${user}.`);
+});
+
+app.post("/register",(req,res)=>{
+    let {user,pass}=req.body;
+    console.log(req.body);
+    res.send(`Satndard POST Response.Welcome ${user}.`)
+})
+
+app.listen(port,()=>{
+    console.log(`App is listening to ${port}.`);
+});
