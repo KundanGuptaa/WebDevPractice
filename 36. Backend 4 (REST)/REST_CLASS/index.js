c6fa8df694d43b469e42d1d7c10c20b8674ef311
Kundan Gupta <kundankumarsobh@gmail.com>
2025-07-16 14:52:53 +0530

    CRUD operation in express

--- 36. Backend 4 (REST)/REST_CLASS/index.js
+++ 36. Backend 4 (REST)/REST_CLASS/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const {v4: uuidv4 }=require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,14 +13,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts=[
     {
+        id:uuidv4(),
         username:"kundan gupta",
         content:"I Love Coding"
     },
     {
+        id:uuidv4(),
         username:"kundan kumar",
         content:"Hard work is important to achieve success"
     },
     {
+        id:uuidv4(),
         username:"kundan",
         content:"I Love new technologies"
     }
@@ -27,7 +31,28 @@ let posts=[
 app.get("/posts", (req, res) => {
   res.render("index.ejs", {posts});
 });
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs", {posts});
+});
 
+app.post("/posts",(req,res)=>{
+  let{username,content}=req.body; 
+  let id=uuidv4();
+  posts.push({id,username,content});
+  res.redirect("/posts");
+});
+app.get("/posts/:id",(req,res)=>{
+  let{id}=req.params;
+  let post=posts.find((p)=>id===p.id);
+  res.render("show.ejs",{post});
+});
+
+app.patch("/posts/:id",(req,res)=>{
+    let id =req.params;
+    let newContent=req.body.content;
+    console.log(newContent);
+    res.send("patch request working");
+})
 
 app.listen(port, () => {
   console.log(`App is listening to port ${port}`);
