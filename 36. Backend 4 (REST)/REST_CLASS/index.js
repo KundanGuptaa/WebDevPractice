b663d13faafeb77a97d3885e055b5781f63c51d6
Kundan Gupta <kundankumarsobh@gmail.com>
2025-07-17 12:48:26 +0530

    CRUD Operation Using RESTful API

--- 36. Backend 4 (REST)/REST_CLASS/index.js
+++ 36. Backend 4 (REST)/REST_CLASS/index.js
@@ -2,57 +2,74 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
-const {v4: uuidv4 }=require("uuid");
+const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-let posts=[
-    {
-        id:uuidv4(),
-        username:"kundan gupta",
-        content:"I Love Coding"
-    },
-    {
-        id:uuidv4(),
-        username:"kundan kumar",
-        content:"Hard work is important to achieve success"
-    },
-    {
-        id:uuidv4(),
-        username:"kundan",
-        content:"I Love new technologies"
-    }
+let posts = [
+  {
+    id: uuidv4(),
+    username: "kundan gupta",
+    content: "I Love Coding",
+  },
+  {
+    id: uuidv4(),
+    username: "kundan kumar",
+    content: "Hard work is important to achieve success",
+  },
+  {
+    id: uuidv4(),
+    username: "kundan",
+    content: "I Love new technologies",
+  },
 ];
 app.get("/posts", (req, res) => {
-  res.render("index.ejs", {posts});
+  res.render("index.ejs", { posts });
 });
 app.get("/posts/new", (req, res) => {
-  res.render("new.ejs", {posts});
+  res.render("new.ejs", { posts });
 });
 
-app.post("/posts",(req,res)=>{
-  let{username,content}=req.body; 
-  let id=uuidv4();
-  posts.push({id,username,content});
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  let id = uuidv4();
+  posts.push({ id, username, content });
   res.redirect("/posts");
 });
-app.get("/posts/:id",(req,res)=>{
-  let{id}=req.params;
-  let post=posts.find((p)=>id===p.id);
-  res.render("show.ejs",{post});
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  console.log(id);
+  let post = posts.find((p) => id === p.id);
+  res.render("show.ejs", { post });
 });
 
-app.patch("/posts/:id",(req,res)=>{
-    let id =req.params;
-    let newContent=req.body.content;
-    console.log(newContent);
-    res.send("patch request working");
-})
+app.patch("/posts/:id", (req, res) => {
+  let newContent = req.body.content;
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  console.log(post);
+  res.redirect("/posts");
+  res.send("patch request working");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", { post });
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => id !== p.id);
+  res.redirect("/posts");
+});
 
 app.listen(port, () => {
   console.log(`App is listening to port ${port}`);
